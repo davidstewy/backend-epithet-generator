d5940c84958ce683460cb872ac41dcdf356b7a0f
David Stewart <davidstewy@gmail.com>
2019-02-07 23:03:45 -0500

    added docstrings, trimmed all_vocab

--- submissions/sprint_a/helpers.py
+++ submissions/sprint_a/helpers.py
@@ -13,6 +13,7 @@ class FileManager:
 
     @staticmethod
     def read_json(path, mode='r', *args, **kwargs):
+        """Opens and reads data from path, returns json"""
         with open(path, mode=mode, *args, **kwargs) as handle:
             return json.load(handle)
 
@@ -24,12 +25,16 @@ class Vocabulary:
 
     @classmethod
     def from_file(cls, path, *args, **kwargs):
+        """Finds the extension of a file then runs the appropriate
+        class method based on that extension"""
         extension = cls.files.get_extension(path)
         representation = cls.strategies(extension)(path, *args, **kwargs)
         return representation
 
     @classmethod
     def from_json(cls, path, fields=True, *args, **kwargs):
+        """Stores json data passed to it and returns data if fields=False,
+        if True it will return a tuple of data and data's keys"""
         data = cls.files.read_json(path, *args, **kwargs)
         if fields:
             representation = (data, data.keys())
@@ -39,6 +44,7 @@ class Vocabulary:
 
     @classmethod
     def strategies(cls, file_extension, intent='read'):
+        """Runs a class method based on the extension passed"""
         input_strategies = {'json': cls.from_json}
         if intent is 'read':
             return input_strategies[file_extension]
@@ -46,6 +52,8 @@ class Vocabulary:
 
 class EpithetGenerator:
     def create_insult(path):
+        """Pulls one random word from each Column/Key and formats
+        them into a phrase/string"""
         my_dict = Vocabulary.from_file(path, fields=False)
         my_phrase = '{} {} {}'.format(random.choice(my_dict["Column 1"]),
                                       random.choice(my_dict["Column 2"]),
@@ -53,5 +61,6 @@ class EpithetGenerator:
         return my_phrase
 
     def all_vocab(path):
+        """Returns json data from path"""
         my_dict = Vocabulary.from_file(path, fields=False)
         return my_dict
